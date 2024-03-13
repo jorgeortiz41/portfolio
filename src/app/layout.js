55a64e24649d2df8664262beab9fb5bb61afd129
jorgeortiz41 <jorge.ortiz41@upr.edu>
2024-03-13 11:14:41 -0400

    added vercel speedInsights

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata = {
   title: "Jortiz",
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <div>{children}</div>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
