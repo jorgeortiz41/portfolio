cf0a76262572ffa0e81e3726109e34f91a0bce15
jorgeortiz41 <jorge.ortiz41@upr.edu>
2024-03-24 03:06:39 -0400

    added leetcode 75 to projects

--- src/app/data/data.js
+++ src/app/data/data.js
@@ -60,6 +60,14 @@ export const projects = [
     tags: ["Next.js", "Tailwind CSS", "HTML"],
     link: "https://github.com/jorgeortiz41/e-commerce-product",
   },
+  {
+    title: "LeetCode 75(IN PROGRESS)",
+    image: "/leetcode.png",
+    description:
+      "A collection of 75 LeetCode problems that cover a wide range of topics. The problems are solved in JavaScript.",
+    tags: ["JavaScript", "LeetCode"],
+    link: "https://github.com/jorgeortiz41/LEETCODE_75/tree/main",
+  },
 ];
 
 export const GithubLogo = (
